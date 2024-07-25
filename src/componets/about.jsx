d3bc39a3db86c6e89fa6d8e1f6f58e51e14b5de1
unknown <brechterben8@gmail.com>
2024-07-25 12:20:26 -0500

    images insert

--- src/componets/about.jsx
+++ src/componets/about.jsx
@@ -12,8 +12,8 @@ function About() {
 
         <div className="about-text-container">
 
-          <h1>Summmer Gozum</h1>   
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos facere id iusto aperiam, quis velit asperiores sed non obcaecati illo doloremque amet distinctio explicabo iure nam quibusdam sapiente? Aspernatur, maxime.</p>   
+          <h1 className='about-header'>Summmer Gozum</h1>   
+          <p className='about-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos facere id iusto aperiam, quis velit asperiores sed non obcaecati illo doloremque amet distinctio explicabo iure nam quibusdam sapiente? Aspernatur, maxime.</p>   
         </div>
       </div>
     )

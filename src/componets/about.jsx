39e5f9d8ae2d6d130cd7beb1540f692f444d9ce3
Benjamin Brechter <benbrechter3@gmail.com>
2024-08-05 13:22:07 -0500

    touch ups

--- src/componets/about.jsx
+++ src/componets/about.jsx
@@ -1,20 +1,14 @@
-import img from '../assets/placeholder.jpg'
-import summer from '../assets/summers img/image11.jpeg'
+import summer from '../assets/summers img/image2.jpeg'
+import summer2 from '../assets/summers img/image10.jpeg'
+import summer3 from '../assets/summers img/image11.jpeg'
 
 function About() {
 
     return (
       <div className="about-container">
-        <div>
-        <img src={img} alt="" />
-        <h2>Artist</h2>
-        </div>
-        
-
         <div className="about-text-container">
-
-          <img src={summer} alt="" className='about-summer-img'/>  
-          <p className='about-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos facere id iusto aperiam, quis velit asperiores sed non obcaecati illo doloremque amet distinctio explicabo iure nam quibusdam sapiente? Aspernatur, maxime.</p>   
+          <h1 className='name'>Summer Gozum</h1>  
+          <h1 className='title'> Musician, <br /> Artist</h1>
         </div>
       </div>
     )
--- src/componets/contact.jsx
+++ src/componets/contact.jsx
@@ -5,8 +5,8 @@ function Contact() {
       <div>
         <div className="contact-container">
           <div>
-            <h1>Lets Talk</h1>
-            <h3>these will be socials that are clickable no lie</h3>
+            <h1 className = 'contact-header'>Lets Work Together</h1>
+            <h3 className = 'contact-text'>Feel free to leave me a message via the form, or you can get a hold of me at summergozum@gmail.com <br /><br />Let's create somethionf beautiful</h3>
           </div>
        
           <div className="form-container">
